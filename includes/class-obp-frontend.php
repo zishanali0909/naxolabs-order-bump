bcf3bc679e3036b863cdb3f8c4c3db965de9f3e5
zishanali0909 <zishanali0909@gmail.com>
2026-09-16 23:29:45 +0530

    Fix: use large image size for better quality when width is increased

--- includes/class-obp-frontend.php
+++ includes/class-obp-frontend.php
@@ -191,7 +191,7 @@ class OBP_Frontend {
                 if ( $img_type === 'custom' && $img_custom_url ) {
                     $img_src = esc_url($img_custom_url);
                 } else {
-                    $img_src = wp_get_attachment_image_url( $product->get_image_id(), 'medium' );
+                    $img_src = wp_get_attachment_image_url( $product->get_image_id(), 'large' );
                 }
                 if ( $img_src ) {
                     $w = max(40, min(300, $img_width));
